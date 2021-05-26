8594160938812147936a60022dfdca0e9e7d7de5
jose andrango <joseandrango64@gmail.com>
2021-05-26 08:11:55 -0500

    Muestro el valor del numero ingresado

---
+++ operaciones.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    // Variables a utilizar
+    long int Resultado=0;
+    long int Factorial;
+
+    // Solicitar numero para calcular el factorial
+    printf("Introduzca No. para calcular el factorial ...: ");
+    scanf(" %ld", &Factorial);
+    // Calcular el factorial del numero solicitado
+    Resultado = 1;
+    while(Factorial > 1) {
+       Resultado *= Factorial;
+       printf(" %ld x",Factorial);
+       Factorial--;
+    }
+    printf(" 1 = %ld\n\n",Resultado);
+    printf("Gracias por utilizar este programa");  
+  system("PAUSE");
+  return 0;
+}
